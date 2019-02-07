93f9d1595de600d4f96b018968f2f164d012ffb6
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2019-02-07 15:37:47 -0500

    Debugged control flow error

--- GEDParser.java
+++ GEDParser.java
@@ -49,21 +49,18 @@ public class GEDParser
             //Split input str into array
             String[] splitStr = str.split(" ");
 
-            String currLevel = splitStr[0];
-            // System.out.println(currLevel);
+            int currLevel = Integer.valueOf(splitStr[0]);
 
             //Check which level and use appropriate method
             //Switch to cases later to prettify? 
             System.out.println("Current level: " + currLevel);
-            if (currLevel == "0") {
-                System.out.println("Entering Level0 method");
+            if (currLevel == 0) {
                 Level0(str);
-            } else if(currLevel == "1") {
+            } else if(currLevel == 1) {
                 Level1(str);
-            } else if(currLevel == "2") {
+            } else if(currLevel == 2) {
                 Level2(str);
             } else {
-                // Currently every call goes to else
                 InvalidLevel(str);
             }
 
@@ -86,6 +83,8 @@ public class GEDParser
     public static void Level1(String str){
         String[] words = str.split(" ");
 
+        String tag = words[1];
+        System.out.println("Words[1] is: " + tag);
         boolean isOneTag = Arrays.stream(one_tags).anyMatch(words[1]::equals);
 
         if(isOneTag){
@@ -96,6 +95,8 @@ public class GEDParser
     public static void Level2(String str){
         String[] words = str.split(" ");
 
+        String tag = words[1];
+        System.out.println("Words[1] is: " + tag);
         boolean isTwoTag = Arrays.stream(two_tags).anyMatch(words[1]::equals);
 
         if(isTwoTag){
