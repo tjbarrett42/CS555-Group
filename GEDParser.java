36ba922761c2a7c39cac1f9494c311425b2db3df
Timothy Barrett <tbarrett@stevens.edu>
2019-02-07 15:17:21 -0500

    int to string

--- GEDParser.java
+++ GEDParser.java
@@ -49,14 +49,14 @@ public class GEDParser
             //Split input str into array
             String[] splitStr = str.split(" ");
 
-            int currLevel = splitStr[0];
+            String currLevel = splitStr[0];
 
             //Check which level and use appropriate method
             //Switch to cases later to prettify? 
             if(currLevel == "0") Level0(currLevel);
             else if(currLevel == "1") Level0(currLevel);
             else if(currLevel == "2") Level0(currLevel);
-            
+
         }
     }
     public static void Level0(String str){
