c874b3595f37d81d47ceeca45fdeef77377b1b8f
Dylan DiGeronimo <dylandigeronimo1@gmail.com>
2019-03-03 19:20:24 -0500

    Refactored MarriageBeforeDeath and MarriageBeforeDivorce

--- MarriageBeforeDivorce.java
+++ MarriageBeforeDivorce.java
@@ -1,54 +1,63 @@
 // Dylan DiGeronimo
 // I pledge my honor that I have abided by the Stevens Honor System
 
-public class MarriageBeforeDivorce extends GEDParser {
+public class MarriageBeforeDivorce {
 
-    // I'm pulling the basic structure for this from my group's project
-    // I'm not sure where it will fit in to the full structure, so I will have it run on the already stored families rather than on the GEDCOM file
+	// Returns false if the first date string is later than the second date string
+	public static boolean isBefore(String dateString1, String dateString2) {
+		// Split date strings into arrays
+		String[] dateStringArray1 = dateString1.split("-");
+		String[] dateStringArray2 = dateString2.split("-");
+		// Convert string arrays to integer arrays
+		int[] d1 = new int[dateStringArray1.length];
+		for (int i = 0; i < dateStringArray1.length; i++) {
+			d1[i] = Integer.parseInt(dateStringArray1[i]);
+		}
+		int[] d2 = new int[dateStringArray2.length];
+		for (int i = 0; i < dateStringArray2.length; i++) {
+			d2[i] = Integer.parseInt(dateStringArray2[i]);
+		}
+		if (d1[2] > d2[2]) {
+			// d1 year after d2 year
+			return false;
+		} else if (d1[2] == d2[2]) {
+			// Same year, have to check month
+			if (d1[0] > d2[0]) {
+				// d1 month after d2 month
+				return false;
+			} else if (d1[0] == d2[0]) {
+				// Same month, have to check day
 
-    public static boolean CheckMarriageBeforeDivorce(String[] family) {
-    	if ((family[1] == "NA" && family[2] == "NA") || (family[1] != "NA" && family[2] == "NA")) {
-    		// Valid to be neither married nor divorced or married but not divorced
-    		return true;
-        } else if (family[1] == "NA" && family[2] != "NA") {
-        	// Invalid to be not married but divorced
-        	return false;
-        } else if (family[1] == family[2]) {
-            // Married and divorced on the same day
-            return true;
-        } else {
-            // Get both dates as ["MM", "DD"m "YYYY"]
-            String[] mar = family[1].split("-");
-            String[] div = family[2].split("-");
-            if (Integer.parseInt(mar[2]) > Integer.parseInt(div[2])) {
-                // Marriage year > divorce year
-                return false;
-            } else if (mar[2] == div[2]) {
-                // Same year, have to check month
+				if (d1[1] > d2[1]) {
+					// d1 day after d2 day
+					return false;
+				} else {
+					// Same day or d1 before d2
+					return true;
+				}
+			} else {
+				// d1 month < d2 month
+				return true;
+			}
+		} else {
+			// d1 year < d2 year
+			return true;
+		}
+	}
 
-                if (Integer.parseInt(mar[0]) > Integer.parseInt(div[0])) {
-                    // Marriage month > divorce month
-                    return false;
-                } else if (mar[0] == div[0]) {
-                    // Same month, have to check day
-                    
-                    if (Integer.parseInt(mar[1]) > Integer.parseInt(div[1])) {
-                        // Marriage day > divorce day
-                        return false; 
-                    } else {
-                        // Same day or marriage < divorce
-                        return true;
-                    }
-
-                } else {
-                    // Marriage month < divorce month
-                    return true;
-                }
-
-            } else {
-                // Marriage year < divorce year
-                return true;
-            }
-        }
-    }
+	public static boolean CheckMarriageBeforeDivorce(String[] family) {
+		if ((family[1] == "NA" && family[2] == "NA") || (family[1] != "NA" && family[2] == "NA")) {
+			// Valid to be neither married nor divorced or married but not divorced
+			return true;
+		} else if (family[1] == "NA" && family[2] != "NA") {
+			// Invalid to be not married but divorced
+			return false;
+		} else if (family[1] == family[2]) {
+			// Married and divorced on the same day
+			return true;
+		} else {
+			// Return whether the marriage happened before the divorce or not
+			return isBefore(family[1], family[2]);
+		}
+	}
 }
